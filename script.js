5caa0a30a31aa5faa8178ff1da237468255c91bc
alvarodev12 <alsouza23@outlook.com>
2025-04-04 20:14:41 -0300

    Site Clone Nike

---
+++ script.js
@@ -0,0 +1,8 @@
+let body = document.querySelector("body")
+let tenis = document.querySelector(".img-tenis")
+
+function mudarVisual(cor, imagem) {
+
+    body.style.background = cor
+    tenis.src = imagem
+}
